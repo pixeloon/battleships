379925151e3765dae2b35e2b4a310c1465ebbc3b
Gunther Schneider <pixeloon@gmail.com>
2016-04-03 10:51:35 -0700

    player can populate game board

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 var express = require('express');
 var app = express();
+var locus = require("locus");
 
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var grid = [{player:1},{player: 2}];
+var game = [];
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // io / socket connection
 io.on('connection', function(socket) {
-
-
-
+    socket.on("player joined", function(player) {
+        game.push({ player: player, socketId: socket.id });
+        game.id++;
+        if (game.length === 2) {
+            io.emit("start game", game);
+        }
+    });
 });
 
-
+// eval(locus);
 
 app.get("/", (req, res) => {
-    res.render("index", { grid: grid });
+    res.render("index", { game: game });
 });
 
 server.listen(3000, () => {
-  console.log("Listening on port 3000 ...");
+    console.log("Listening on port 3000 ...");
 });
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,7 +1,41 @@
 $(function() {
     console.log("sanity check");
     var socket = io(); // initiate handshake
-    drawGrid();
+    var player = "";
+    var ships = [];
+    var A = { location: [] };
+    var B = { location: [] };
+    var C = { location: [] };
+    var D1 = { location: [] };
+    var D2 = { location: [] };
+
+
+
+
+
+
+    $(".start").on('click', () => {
+        player = prompt("Please enter your name");
+        $(".start").text("Hello " + player + "!  Once a second player has joined, the game starts.");
+        socket.emit("player joined", player);
+    });
+
+    socket.on("start game", game => {
+        $(".start").text("Players: " + game[0].player + " and " + game[1].player);
+        $(".messages").text("To place your ships, select the right number of tiles, enter the matching letters and click submit.");
+        $(".selection").append("<button type='submit' disable>Submit Ships</button");
+        drawGrid();
+    });
+
+    $(".selection").on('click', () => {
+        var selection = document.querySelectorAll('#tile');
+        // debugger
+        Array.prototype.slice.call(selection).forEach(el => {
+            el.removeEventListener("click", clickHandler);
+            // ships.push(el.value);
+        });
+    });
+
 
     function drawGrid() {
         var location;
@@ -25,6 +59,7 @@ $(function() {
                 tile.style.backgroundColor = ("lightgray");
             }
             tile.addEventListener("click", clickHandler);
+            tile.addEventListener("dblclick", dblclickHandler);
             tile.setAttribute("id", "tile");
             $('.maingrid').append(tile);
 
@@ -42,7 +77,6 @@ $(function() {
                     rowCount++;
                 }
             }
-            // location = arrLocation[rowCount] + counter;
         }
         tiles = document.querySelectorAll('#tile');
         Array.prototype.slice.call(tiles).forEach((el) => {
@@ -57,11 +91,70 @@ $(function() {
             el.setAttribute('data-location', row + col);
             counter++;
         });
-
     }
 
-    function clickHandler() {
-        console.log("You clicked");
+    function clickHandler(e) {
+
+        var location;
+
+        if ($('#aircraftcarrier').is(':checked')) {
+            $(e.target).text("A");
+            location = $(e.target).attr("data-location");
+            if (A.location.length < 5) {
+                A.location.push(location);
+            } else {
+                $(e.target).text("");
+                alert("Aircraft Carrier is deployed. Select a different ship, or click submit.");
+            }
+        }
+        if ($('#battleship').is(':checked')) {
+            $(e.target).text("B");
+            location = $(e.target).attr("data-location");
+            if (B.location.length < 4) {
+                B.location.push(location);
+            } else {
+                $(e.target).text("");
+                alert("Battleship is deployed. Select a different ship, or click submit.");
+            }
+        }
+        if ($('#cruiser').is(':checked')) {
+            $(e.target).text("C");
+            location = $(e.target).attr("data-location");
+            if (C.location.length < 3) {
+                C.location.push(location);
+            } else {
+                $(e.target).text("");
+                alert("Cruiser is deployed. Select a different ship, or click submit.");
+            }
+        }
+        if ($('#destroyer1').is(':checked')) {
+            $(e.target).text("D");
+            location = $(e.target).attr("data-location");
+            if (D1.location.length < 2) {
+                D1.location.push(location);
+            } else {
+                $(e.target).text("");
+                alert("Destroyer 1 is deployed. Select a different ship, or click submit.");
+            }
+        }
+        if ($('#destroyer2').is(':checked')) {
+            $(e.target).text("D");
+            location = $(e.target).attr("data-location");
+            if (D2.location.length < 2) {
+                D2.location.push(location);
+            } else {
+                $(e.target).text("");
+                alert("Destroyer 2 is deployed. Select a different ship, or click submit.");
+            }
+        }
+
+        console.log("Location: " + $(e.target).attr("data-location"));
+        console.log("Ships: " + JSON.stringify(A) + JSON.stringify(B) + JSON.stringify(C) + JSON.stringify(D1) + JSON.stringify(D2));
+        this.removeEventListener("click", clickHandler);
+    }
+
+    function dblclickHandler(e) {
+        $(e.target).css("background", "red");
     }
 
 });
